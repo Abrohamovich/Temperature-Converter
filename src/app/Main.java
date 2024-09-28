06dfc2a3e73a8d624c505c95fab4e44e332811e9
Mykyta Herasymov <gerasimov08nikita@gmail.com>
2024-09-28 14:09:51 +0300

    Add Fahrenheit to Celsius convertation

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,14 @@ public class Main {
 
         System.out.println("App for temperature transformation");
 
+        double fahrenheit = 97.8d;
+        double celsius = FtoC(fahrenheit);
+
+        System.out.println(fahrenheit + "F is equal to " + celsius + "C");
+    }
+
+    private static double FtoC(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
     }
 
 }
